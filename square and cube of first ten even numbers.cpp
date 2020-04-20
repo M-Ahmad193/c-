a6a236e75f534e790ff19be5299a56ffca353e87
M-Ahmad193 <62458787+M-Ahmad193@users.noreply.github.com>
2020-04-20 18:16:37 +0500

    square and cube of first ten even numbers
     square and cube of first ten even numbers

---
+++ square and cube of first ten even numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	double sum;
+	
+for(int i=0;i<=20;i++){
+i%2==0;
+
+	sum=sum+i;
+	
+
+}
+cout<<"square of 1st 10 even number="<<sum*sum<<endl;
+cout<<"cube of 1st 10 even number= "<<sum*sum*sum<<endl;
+return 0;
+
+}
